0c211f3d7dfd4df1f92185e9840dec23430fb17d
Shishir Paudel <sishirpaudel7@gmail.com>
2025-08-07 17:57:32 -0400

    about us

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <HeroComponent />
-      asd
+      <AboutUsComponent />
     </div>
   );
 }
@@ -28,11 +28,64 @@ const HeroComponent = () => {
 
       <div className="absolute bottom-0 left-0 right-0 flex items-center justify-center">
         <div className="bg-white text-black py-4 px-6 rounded-t-[30px] flex justify-around items-center gap-6 w-[70%] z-20">
-          <img src="https://www.thecloroxcompany.com/wp-content/uploads/2022/08/TCC_Stacked_1200x1200.png" alt="Woox" className="w-20 h-20 object-contain" />
-          <img src="https://www.vhv.rs/dpng/d/8-81706_famous-company-logos-png-transparent-png.png" alt="Exodus" className="w-20 h-20 object-contain" />
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Southern_company_logo.png/250px-Southern_company_logo.png" alt="BitGo" className="w-20 h-20 object-contain" />
-          <img src="https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdjkxNC1uaW5nLTE1YS5wbmc.png" alt="Ankr" className="w-20 h-20 object-contain" />
-          <img src="https://www.pngkey.com/png/full/141-1417312_generic-branding-generic-company-logo-png.png" alt="Metamask" className="w-20 h-20 object-contain" />
+          <img
+            src="https://www.thecloroxcompany.com/wp-content/uploads/2022/08/TCC_Stacked_1200x1200.png"
+            alt="Woox"
+            className="w-20 h-20 object-contain"
+          />
+          <img
+            src="https://www.vhv.rs/dpng/d/8-81706_famous-company-logos-png-transparent-png.png"
+            alt="Exodus"
+            className="w-20 h-20 object-contain"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Southern_company_logo.png/250px-Southern_company_logo.png"
+            alt="BitGo"
+            className="w-20 h-20 object-contain"
+          />
+          <img
+            src="https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdjkxNC1uaW5nLTE1YS5wbmc.png"
+            alt="Ankr"
+            className="w-20 h-20 object-contain"
+          />
+          <img
+            src="https://www.pngkey.com/png/full/141-1417312_generic-branding-generic-company-logo-png.png"
+            alt="Metamask"
+            className="w-20 h-20 object-contain"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+const AboutUsComponent = () => {
+  return (
+    <section className="py-16 px-4 md:px-20 bg-white text-black flex justify-center">
+      <div className="flex md:w-[70%] flex-col lg:flex-row items-center lg:items-start gap-10">
+        
+        <div className="flex gap-4 w-full lg:w-1/2 justify-center">
+          <img
+            src="/aboutus_left.png"
+            alt="Team Left"
+            className="w-1/3 rounded-lg shadow-md object-cover"
+          />
+          <img
+            src="/aboutus_middle.png"
+            alt="Team Middle"
+            className="w-1/3 rounded-lg shadow-md object-cover"
+          />
+          <img
+            src="/aboutus_right.png"
+            alt="Team Right"
+            className="w-1/3 rounded-lg shadow-md object-cover"
+          />
+        </div>
+
+        <div className="w-full lg:w-1/2 h-full  flex flex-col items-center justify-center text-center lg:text-left">
+          <h2 className="text-4xl font-bold text-green-600 mb-6">About US</h2>
+          <p className="text-lg leading-relaxed text-gray-800">
+            Concept Softworks is a software company, we develop custom built software for clients – covering everything from financial institutions & medical companies all the way to tech companies and government organizations.
+          </p>
         </div>
       </div>
     </section>
