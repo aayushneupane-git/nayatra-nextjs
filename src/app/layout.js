9a7d292363596c8b7119d4385e41a3946d113f89
Aayush Neupane <aayushneupane55@gmail.com>
2025-08-11 14:13:04 -0400

    Normal

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,49 +7,18 @@ import Footer from "@/components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  metadataBase: new URL("https://nepsoft.org"), // ensures relative URLs become absolute
-  title: "NepSoft",
-  description: "This is where the future of software development",
-  keywords: [
-    "software",
-    "development",
-    "agency",
-    "web development",
-    "mobile apps",
-  ],
-  authors: [{ name: "NepSoft", url: "https://nepsoft.org" }],
-  creator: "NepSoft",
-
+  title: 'NepSoftSolutions',
+  description: 'A description of NepSoftSolutions.',
   openGraph: {
-    title: "NepSoft",
-    description: "This is where the future of software development",
-    url: "https://nepsoft.org",
-    siteName: "NepSoft",
-    type: "website",
-    images: [
-      {
-        url: "/LOGO.png",
-        width: 1200,
-        height: 630,
-        alt: "NepSoft",
-      },
-    ],
+    title: 'NepSoftSolutions',
+    description: 'A description of NepSoftSolutions for social media.',
+    images: ['/logo11.png'],
   },
-
   twitter: {
-    card: "summary_large_image",
-    title: "NepSoft",
-    description: "This is where the future of software development",
-    images: ["/LOGO.png"],
-  },
-
-  icons: {
-    icon: [
-      { url: "/logo_white.png", sizes: "any" },
-    ],
-    apple: [
-      { url: "/logo_white.png", sizes: "180x180" }, 
-    ],
+    card: 'summary_large_image',
+    title: 'NepSoftSolutions',
+    description: 'A description of NepSoftSolutions for Twitter.',
+    images: ['/logo11.png'],
   },
 };
 
