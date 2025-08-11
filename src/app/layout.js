8b76f0a774a7e05db36e4fecddca65fa2d6dc21b
Aayush Neupane <aayushneupane55@gmail.com>
2025-08-11 16:00:56 -0400

    Meta Image for Search Engine

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
-import ogImage from "../../public/logo3.png";
+import ogImage from "../../public/favicon.ico";
 import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -54,31 +54,11 @@ export const metadata = {
       "NepSoft Solutions builds SEO-optimized websites, mobile apps, and AI-powered software to accelerate your business success.",
     images: [ogImage.src],
   },
-  icons: {
-    icon: ogImage.src,
-    apple: ogImage.src,
-  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="icon" href="/logo_white.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/logo_white.png" />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/logo_white.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/logo_white.png"
-        />
-      </head>
       <body className="font-sans">
         <link
           href="https://fonts.googleapis.com/css2?family=Andada+Pro:ital,wght@0,400..840;1,400..840&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
