6bc80f8042f2202326d9a97c44889dec0d094dc1
Shishir Paudel <sishirpaudel7@gmail.com>
2025-08-11 14:59:11 -0400

    font and favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,19 +8,47 @@ import Footer from "@/components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
+  metadataBase: new URL("https://nepsoft.org"), // ensures relative URLs become absolute
   title: "NepSoft",
   description: "This is where the future of software development",
-  keywords: "software, development, agency, web development, mobile apps",
+  keywords: [
+    "software",
+    "development",
+    "agency",
+    "web development",
+    "mobile apps",
+  ],
   authors: [{ name: "NepSoft", url: "https://nepsoft.org" }],
   creator: "NepSoft",
+
   openGraph: {
     title: "NepSoft",
     description: "This is where the future of software development",
     url: "https://nepsoft.org",
     siteName: "NepSoft",
+    type: "website",
+    images: [
+      {
+        url: "/LOGO.png",
+        width: 1200,
+        height: 630,
+        alt: "NepSoft",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "NepSoft",
+    description: "This is where the future of software development",
+    images: ["/LOGO.png"],
+  },
+
+  icons: {
+    icon: [{ url: "/logo_white.png", sizes: "any" }],
+    apple: [{ url: "/logo_white.png", sizes: "180x180" }],
   },
 };
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
@@ -29,12 +57,12 @@ export default function RootLayout({ children }) {
           href="https://fonts.googleapis.com/css2?family=Andada+Pro:ital,wght@0,400..840;1,400..840&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
           rel="stylesheet"
         />
-          <div>
-            <Navbar />
-          </div>
+        <div>
+          <Navbar />
+        </div>
 
-          {children}
-          <Footer/>
+        {children}
+        <Footer />
       </body>
     </html>
   );
